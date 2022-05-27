fc2eae730973532773c8ee42492fdeec6af56097
Johnny <johnny719top@gmail.com>
2022-05-27 10:29:04 +0800

    fix: about and experiece content

--- src/containers/about.js
+++ src/containers/about.js
@@ -15,7 +15,7 @@ export function AboutContainer() {
               <About.TitleOne>Hello, I am </About.TitleOne>
               <About.TitleTwo>React Developer</About.TitleTwo>
               <About.Article>
-                我是林宗毅，畢業自元智大學資訊工程學系，具備超過三年實務開發經驗。熟悉React，擁有開發大型專案的經歷。
+                我是林宗毅，畢業自元智大學資訊工程學系，具備超過三年實務開發經驗。熟悉React，擁有開發大型專案的經歷，包含後台CMS及電商APP。
               </About.Article>
               <About.Article>
                 在工作上遇到問題會與同事討論，喜歡一起追求乾淨且可維護的程式碼，看到有困難的同事，會以引導式的去幫助一同進步。我的學習能力快速，在短時間內就能適應陌生的技術及專案，並且快速地進入開發。我對程式語言有很高的熱忱，只要開始實作新的功能項，就會很專心的投入，享受中間的過程，直到完成目標。
